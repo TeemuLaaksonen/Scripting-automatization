2a873afa7859378f98d9e4eae7019d70f5605f1f
Teemu Laaksonen <ab3688@student.jamk.fi>
2022-10-18 11:17:17 +0300

    1st task done

---
+++ powershell/powershell-5/Task01.ps1
@@ -0,0 +1,9 @@
+param (
+    $filename
+)
+
+if (Test-Path -Path $filename) {
+    Import-Csv $filename | ForEach-Object {Write-Host("Hello my colleague " + $_.Firstname + " " + $_.Lastname + ". This is your new account: `n" +  (($_.Lastname.ToLower()).Substring(0,4)) + (($_.Firstname.ToLower()).Substring(0,2)))
+    $i++}
+}
+Write-Host("$i accounts created successfully") -ForegroundColor Green
