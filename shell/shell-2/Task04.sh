02e4f824a0b27e3cae12e1802047082d56d99ee3
AB3688 <ab3688@student.jamk.fi>
2022-11-04 12:36:04 +0200

    Shell-2 tasks done

--- shell/shell-2/Task04.sh
+++ shell/shell-2/Task04.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+#script must be run with sudo "sudo ./Task04.sh"
+
+echo "Give the name of a new user:"
+read -r USERNAME
+
+if id -u "$USERNAME" > /dev/null 2>&1
+then
+    echo "User exists, script exits."
+else
+    echo "User doesn't exist, creating user $USERNAME."
+    useradd -m -s /bin/bash "$USERNAME"
+fi
--- shell/shell-2/Task05.sh
+++ shell/shell-2/Task05.sh
@@ -1,1 +1,32 @@
 #!/bin/bash
+
+while true; do
+
+echo "Give operation (-, +, * or /) or 'exit' to exit script:"
+read -r OPERAND
+
+if [ "$OPERAND" = "exit" ]
+then
+break
+fi
+
+echo "Give first value:"
+read -r VALUE1 
+
+echo "Give second value:"
+read -r VALUE2
+    if [ "$OPERAND" = "+" ]
+        then
+            RESULT=$((VALUE1+VALUE2))
+    elif [ "$OPERAND" = "-" ]
+        then
+            RESULT=$((VALUE1-VALUE2))
+    elif [ "$OPERAND" = "*" ]
+        then
+            RESULT=$((VALUE1*VALUE2))
+    elif [ "$OPERAND" = "/" ]
+        then
+            RESULT=$((VALUE1/VALUE2))
+    fi
+    echo "Result: $RESULT"
+done
